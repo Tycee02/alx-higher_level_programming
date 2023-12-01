0d968825818e99baf6d6920cbfd7a94a5b31a144
Tycee02 <wakilaminat@gmail.com>
2023-11-30 18:55:47 -0800

    hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import importlib.util
-import dis
-
-spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-module_names = dir(module)
-
-filtered_names = sorted(name for name in module_names if not name.startswith('__'))
-for name in filtered_names:
-    print(name)
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != "and name[1] !=":
+            print(name)
