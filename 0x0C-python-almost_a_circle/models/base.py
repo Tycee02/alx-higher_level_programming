32d8fb1d42aeaa715147435da680bd037e797c07
Tycee02 <wakilaminat@gmail.com>
2024-01-16 02:22:04 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""base.py"""
+
+
+class Base:
+    """
+    The Base class serves as a base class for other classes and includes
+functionality for managing unique identifiers.
+
+    Attributes:
+    __nb_objects (int): A class variable to keep track
+of the number of objects created.
+    id (int): An instance variable representing the
+unique identifier assigned to an object.
+
+    Methods:
+    __init__(self, id=None):
+        Initializes a Base object with a unique identifier.
+        If 'id' is provided, the object is assigned that identifier;
+otherwise, a new identifier is generated.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a Base object with a unique identifier.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
