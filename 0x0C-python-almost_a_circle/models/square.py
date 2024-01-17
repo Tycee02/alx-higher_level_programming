ec07831b3e9f526d3fc8cd0ff0a5c4c45f56524b
Tycee02 <wakilaminat@gmail.com>
2024-01-17 14:46:56 -0800

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,79 +1,54 @@
 #!/usr/bin/python3
-"""
-square.py
-"""
-
+""" Square """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    The Square class represents a square and
-    inherits from the Rectangle class.
-    """
+    """class square extends from rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square object with the specified size, position, and ID.
-        """
+        """class constructor for square"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
-        """
-        Returns a string representation of the Square.
-        """
-        return "[Square] ({}) {}/{} - {}".format(self.id,
+        """str representation of square"""
+        return '[Square] ({}) {}/{} - {}'.format(self.id,
                                                  self.x,
                                                  self.y,
                                                  self.size)
 
     @property
     def size(self):
-        """
-        retrieve size
-        """
+        """retrieve size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-        Set width and height with the same value
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be > 0")
-
+        """set width and height with the same value, value"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """
-        Updates the Square attributes based on the arguments
-or keyword arguments.
-        """
-        if len(args) == 0:
-            for key, value in kwargs.items():
-                self.__setattr__(key, value)
-            return
-
-        try:
-            self.id = args[0]
-            self.size = args[1]
-            self.x = args[2]
-            self.y = args[3]
-        except IndexError:
-            pass
+        """assigns attributes"""
+        if (args):
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.size = j
+                elif i == 2:
+                    self.x = j
+                elif i == 3:
+                    self.y = j
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def to_dictionary(self):
-        """
-        Function that returns the dictionary representation
-of a square
-        """
-        my_dict = {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
-        }
+        """return dictionary representation of square"""
+        my_dict = {}
+        my_dict["id"] = self.id
+        my_dict["size"] = self.size
+        my_dict["x"] = self.x
+        my_dict["y"] = self.y
         return my_dict
