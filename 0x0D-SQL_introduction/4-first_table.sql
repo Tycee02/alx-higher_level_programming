f47a025ed0c1cada62c7175dfbe91f321c40698b
Tycee02 <wakilaminat@gmail.com>
2024-02-14 10:18:43 +0100

    Creating first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Create a table called first_table in the current database
+-- The database will be passed as an argument
+-- The id is INT and name is  VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
