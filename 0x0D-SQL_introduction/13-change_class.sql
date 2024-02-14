c0fc7be6993b174396f4eac116fba3cd7051f57b
Tycee02 <wakilaminat@gmail.com>
2024-02-14 18:37:13 +0100

    removes al records wirh a score <= 5

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- removes all the records with a score <= 5 in the second_table of database
+-- The database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table WHERE score <0 5;
