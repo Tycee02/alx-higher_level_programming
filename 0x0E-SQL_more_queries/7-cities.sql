fedb4ed58569f719c7c30bda6b252ee061ae50a7
Tycee02 <wakilaminat@gmail.com>
2024-02-17 10:59:13 +0100

    creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- create a database 'hbtn_0d_usa'
+-- If the table states already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- create table 'cities' in the database 'hbtn_0d_usa'
+-- description: id INT, auto generated, not null and primary key
+--state_id INT, not null, foreign key and reference to id of states table
+-- name VARCHAR(256) not NULL
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (state_id)
+        REFERENCES hbtn_0d_usa.states(id));
