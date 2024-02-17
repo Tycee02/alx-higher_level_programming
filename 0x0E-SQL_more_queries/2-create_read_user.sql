fa0b5d0f440fd9d79b9b81997a55babafda7930b
Tycee02 <wakilaminat@gmail.com>
2024-02-17 09:52:37 +0100

    creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- creates database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+
+CREATE database IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'localhost'@'user_0d_2'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT TO 'user_0d_2'@'localhost'
+FLUSH PRIVILEGES;
