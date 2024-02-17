8d133d6724b04519db68b4fbdcb125cd59a840c3
Tycee02 <wakilaminat@gmail.com>
2024-02-17 09:16:26 +0100

    creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 -- It should have all priviledges on your MySQL srver
 -- The password should be set to user_0d_pwd
 
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost';
