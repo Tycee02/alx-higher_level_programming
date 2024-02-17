30dcea93b8968287a883dd98403ded3edefe95c1
Tycee02 <wakilaminat@gmail.com>
2024-02-17 12:17:14 +0100

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
--- import the db dump from hbtn_0d_tvshows
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre b linkde
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- lists all shows in hbtn_0d_tvshows that have at least one genre b linkde
+-- Each record should display: tv_shows.title, tv_show_genres.genre_id
+-- Results must be in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can only use one SELECT statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
