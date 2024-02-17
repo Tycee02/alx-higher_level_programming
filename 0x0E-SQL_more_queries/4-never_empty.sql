74e6b89887e1d145d4aed31cac6706482fe5fcdd
Tycee02 <wakilaminat@gmail.com>
2024-02-17 10:17:06 +0100

    creates the table id_not_null on your MySQL server

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- create the 'table id_not_null'
+-- description id INT with the default value 1, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT VALUE 1,
+    name VARCHAR(256));
