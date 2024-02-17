4656c63c08bea9feac31f85db2272e21d3aba69f
Tycee02 <wakilaminat@gmail.com>
2024-02-17 17:06:16 +0100

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,7 +4,7 @@
 -- You can only use one SELECT statement
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+FROM tv_show
+LEFT JOIN tv_shows_genres ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.show is NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
