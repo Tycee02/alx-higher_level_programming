12d0e4b3fcb7a01f1b5835085a83e15d51e80230
Tycee02 <wakilaminat@gmail.com>
2024-02-17 10:47:16 +0100

    creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,12 +2,11 @@
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- create table 'states' (in the database)
--- description: id INT unique, auto generated, can't be null and is a primary key
--- name VARCHAR(256) can not be NULL
+-- create table 'states' in the database 'hbtn_0d_usa'
+-- description: id INT unique, auto generated, not null and primary key
+-- name VARCHAR(256) not NULL
 
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
+    PRIMARY KEY (id));
