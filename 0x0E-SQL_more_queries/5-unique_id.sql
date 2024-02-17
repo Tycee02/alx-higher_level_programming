4d13924eff212c007fbbfab266ba4a745c51dd7a
Tycee02 <wakilaminat@gmail.com>
2024-02-17 10:27:04 +0100

    creates the table unique_id on MySQL serve

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,6 @@
 -- creates the table 'unique_id'
 --description id INT with the default value 1 and unique, name VARCHAR(256)
 
-CREATE TABLE IF NOT EXISTS unique_id
-(
+CREATE TABLE IF NOT EXISTS unique_id(
     id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-);
+    name VARCHAR(256));
