d41a4568fddd160a13a336f18b07fa701d92e920
Tycee02 <wakilaminat@gmail.com>
2024-03-11 22:46:13 +0100

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
-if (firstArg === undefined || secondArg === undefined) {
+if (firstArg === undefined && secondArg === undefined) {
   console.log('undefined is undefined');
 } else if (secondArg === undefined) {
   console.log(`${firstArg} is undefined`);
