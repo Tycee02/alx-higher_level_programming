c6975d55d6c17ad6a464723ad58322492509584b
Tycee02 <wakilaminat@gmail.com>
2024-03-13 23:16:59 +0100

    import an array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data').list;
+const mapped = list.map(function (value, index) { return value * index; });
+console.log(list);
+console.log(mapped);
