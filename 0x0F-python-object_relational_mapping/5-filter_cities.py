d5e8243b0c6a2806cf99de2e3506fb27993cafc8
Tycee02 <wakilaminat@gmail.com>
2024-03-19 20:48:21 +0100

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
                          port=3306)
     cur = db.cursor()
     cmd = """ SELECT cities.id, cities.name, states.name
-          FROM cities
-          JOIN states ON cities.state_id = states.id
-          WHERE states.name = %s
+          FROM states
+          INNER JOIN cities ON state.id = cities.states_id
+          WHERE states.name=%s
           ORDER BY cities.id ASC
           """
-    cur.execute(cmd)
+    cur.execute(cmd, (sys.argv[4]))
     allCities = cur.fetchall()
 
     for city in allCities:
