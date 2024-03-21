814cea113739139186981ae1dec8b4b38c0fda86
Tycee02 <wakilaminat@gmail.com>
2024-03-21 17:04:31 +0100

    First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,10 +3,10 @@
 Script should take 3 args: username, pw, and db name
 Must use SQLAlchemy
 """
-import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
