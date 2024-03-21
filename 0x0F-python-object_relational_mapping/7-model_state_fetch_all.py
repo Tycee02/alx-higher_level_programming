10b36ab286fa57a17d80136ad18e75b30aff521b
Tycee02 <wakilaminat@gmail.com>
2024-03-21 14:06:49 +0100

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-This script lists all State objects
-from the database `hbtn_0e_6_usa`.
+"""List all state object from the db
+Username, password, database name, and state name given as user arg
+Sort ascending order by cities.id
 """
 
 from sys import argv
