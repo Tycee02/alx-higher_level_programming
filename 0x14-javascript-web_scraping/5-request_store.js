24dbcb38237a3e405a7f156d8d38b62554d949e8
Tycee02 <wakilaminat@gmail.com>
2024-04-23 18:37:40 +0100

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,23 +7,22 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 if (!url || !filePath) {
-    console.error('Usage: node get_webpage_content.js <url> <file_path>');
-    process.exit(1);
+  console.error('Usage: node get_webpage_content.js <url> <file_path>');
+  process.exit(1);
 }
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Failed to fetch webpage. Status code:', response.statusCode);
-    } else {
-        fs.writeFile(filePath, body, { encoding: 'utf8' }, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            } else {
-                console.log('Webpage content has been successfully written to', filePath);
-            }
-        });
-    }
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Failed to fetch webpage. Status code:', response.statusCode);
+  } else {
+    fs.writeFile(filePath, body, { encoding: 'utf8' }, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log('Webpage content has been successfully written to', filePath);
+      }
+    });
+  }
 });
-
