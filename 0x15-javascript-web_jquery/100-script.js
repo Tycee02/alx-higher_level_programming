a67b7d4f43622d7ed7d0f7f185908ffeaf966e27
Tycee02 <wakilaminat@gmail.com>
2024-05-05 12:20:11 +0100

    No jQuery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+//  script that updates the text color of the <header> element to red
+//  script must be imported from the head tag not end of the HTML
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
