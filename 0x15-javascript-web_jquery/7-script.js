a2c3b28f7c81902eb212611e0be243fffc09e4f6
Tycee02 <wakilaminat@gmail.com>
2024-05-05 12:05:46 +0100

    Star warrs character

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 // Script that fetches the character name
-// from the URL
+// from the URL: https://swapi-api.alx-tools.com/api/films/?format=json
 
 $.get('https://swapi.co/api/people/5/?format=json', function (data) {
   $('DIV#character').text(data.name);
