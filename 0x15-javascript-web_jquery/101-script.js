c9bb91ddf50ae38942035b3d7b9da5e01a18974d
Tycee02 <wakilaminat@gmail.com>
2024-05-05 12:21:09 +0100

    list, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+// script that add, removes and clear LI elements from the user clicks
+
+$('document').ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
