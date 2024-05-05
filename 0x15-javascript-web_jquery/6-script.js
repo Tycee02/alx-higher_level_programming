d5d9de7eb3d2c608e7d880bae4aac9ec83a68366
Tycee02 <wakilaminat@gmail.com>
2024-05-05 12:01:30 +0100

    change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// Script that updates the text of the header element to New Header!!!
+// When user clicks on DIV#update_header
+
+$('DIV#update_header').click(function () {
+  $('HEADER').text('New Header!!!');
+});
